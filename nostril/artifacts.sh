e2b8b5bdb5a6b26a471e62b168d96a627d942f6f
@RandyMcMillan <randy.lee.mcmillan@gmail.com>
2023-01-04 18:23:02 -0500

    nostril: build automation and nostril message

---
+++ nostril/artifacts.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+DATE=`date +%Y-%m-%d`
+TWITTER_NAME=""
+VERSION="v0.1.1"
+URL="https://github.com/jb55/nostril"
+REPO="https://github.com/jb55/nostril"
+CHECKSUM_SOURCE=""
+PROJECT="nostril"
+SHA256=`shasum -a 256 nostril/nostril | cut -f 1 -d ' '`
+
+# Note GITHUB_ environment variables are populated by Github Actions
+ARTIFACT_BASEURL="https://github.com/${GITHUB_REPOSITORY}/raw"
+ARTIFACT_BRANCH=${GITHUB_REF_NAME}
+
+ENTRY_TO_APPEND="<li><a href='${REPO}/releases/tag/${VERSION}'>${DATE}</a> | <a href='${URL}' class='project-name'>${PROJECT}</a>  | <a href='${REPO}/releases/tag/${VERSION}'>${VERSION}</a> | <a href='${CHECKSUM_SOURCE}'> factory ${SHA256} </a>| <a href='${ARTIFACT_BASEURL}/${ARTIFACT_BRANCH}/${PROJECT}/${PROJECT}-${VERSION}-video.webm'>video proof</a> | <a href='https://github.com/coinkite/bitcoinbinary.org/blob/main/${PROJECT}/artifacts.sh' class="bot">build bot</a></li>"
+
+echo ${ENTRY_TO_APPEND}
