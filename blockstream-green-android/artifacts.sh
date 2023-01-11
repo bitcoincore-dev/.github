8ea2371817d3280691ee72a87aa278efa6676619
@RandyMcMillan <randy.lee.mcmillan@gmail.com>
2023-01-11 10:13:32 -0500

    blockstream-green: symlink for blockstream-green-android: bump version
    Symlinking is for backward compatability with older log entries:
    
    blockstream-green/artifacts.sh -> blockstream-green-android/artifacts.sh
    blockstream-green-android/blockstream-green-3.8.5-video.webm
    blockstream-green-android/blockstream-green-3.8.6-video.webm
    blockstream-green/blockstream-green-3.8.5-video.webm -> blockstream-green-android/blockstream-green-android-3.8.5-video.webm
    blockstream-green/blockstream-green-3.8.6-video.webm -> blockstream-green-android/blockstream-green-android-3.8.6-video.webm
    blockstream-green/blockstream-green-video.webm -> blockstream-green-android/blockstream-green-android-video.webm
    blockstream-green-android/blockstream-green-video.webm
    blockstream-green/steps.sh -> blockstream-green-android/steps.sh

--- blockstream-green-android/artifacts.sh
+++ blockstream-green-android/artifacts.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 DATE=`date +%Y-%m-%d`
-TWITTER_NAME="@Blockstream Green"
+TWITTER_NAME="@Blockstream Green Android"
 URL="https://blockstream.com/"
-VERSION="3.8.6"
+VERSION="3.9.0"
 VERSION_STRING="release_${VERSION}"
 REPO="https://github.com/Blockstream/green_android"
 CHECKSUM_SOURCE="https://github.com/Blockstream/green_android/releases/tag/${VERSION_STRING}"
-PROJECT="blockstream-green"
+PROJECT="blockstream-green-android"
 SHA256=`shasum -a 256 green_android/green/build/outputs/apk/development/release/BlockstreamGreen-v${VERSION}-development-release-unsigned.apk | cut -f 1 -d ' '`
 
 # Note GITHUB_ environment variables are populated by Github Actions
